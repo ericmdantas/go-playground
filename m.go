28ff27e9dd6149b73b26ad7575e7348af5fcb2f5
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-04-14 08:20:57 -0300

    .

--- m.go
+++ m.go
@@ -1,36 +1,57 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"time"
+	"io/ioutil"
+	"net/http"
 )
 
-type ball struct {
-	hits int
+type post struct {
+	UserId int    `json:"userId"`
+	Id     int    `json:"id"`
+	Title  string `json:"title"`
+	Body   int    `json:"body"`
 }
 
-func play(name string, start chan *ball) {
-	for {
-		b := <-start
+const (
+	url1 = "http://jsonplaceholder.typicode.com/posts/1"
+	url2 = "http://jsonplaceholder.typicode.com/posts/2"
+	url3 = "http://jsonplaceholder.typicode.com/posts/3"
+)
 
-		b.hits++
-		fmt.Println(name, b.hits)
+func goGetIt(ps chan post, url string) {
+	var p post
 
-		time.Sleep(time.Millisecond * 100)
+	pl, _ := http.Get(url)
+	b, _ := ioutil.ReadAll(pl.Body)
 
-		start <- b
+	defer pl.Body.Close()
+
+	json.Unmarshal(b, &p)
+
+	ps <- p
+}
+
+func logger(pc1, pc2, pc3 chan post) {
+	select {
+	case <-pc1:
+		fmt.Println(".")
+	case <-pc2:
+		fmt.Println("..")
+	case <-pc3:
+		fmt.Println("...")
 	}
 }
 
 func main() {
-	s := make(chan *ball)
+	pc1 := make(chan post)
+	pc2 := make(chan post)
+	pc3 := make(chan post)
 
-	go play("a", s)
-	go play("b", s)
+	go goGetIt(pc1, url1)
+	go goGetIt(pc2, url2)
+	go goGetIt(pc3, url3)
 
-	s <- new(ball)
-
-	time.Sleep(1 * time.Second)
-
-	<-s
+	logger(pc1, pc2, pc3)
 }
