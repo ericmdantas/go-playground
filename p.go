c5e5d341e7e1a40ccb5545315e004d727c02ef39
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-07-12 08:44:55 -0300

    .

--- p.go
+++ p.go
@@ -1,102 +1,48 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strconv"
-	"sync"
 	"time"
 )
 
 const (
-	amount   = 100
-	fileName = "x.json"
-	url      = "http://jsonplaceholder.typicode.com/todos/"
+	amount = 5000
+	url    = "http://jsonplaceholder.typicode.com/photos/"
 )
 
-type todo struct {
-	ID        int    `json:"id"`
-	UserID    int    `json:"userId"`
-	Title     string `json:"title"`
-	Completed bool   `json:"completed"`
-}
-
-func log(verb, url string) {
-	fmt.Printf("%s -> %s\n", verb, url)
-}
-
-func g() <-chan []todo {
-	var todos []todo
-	var mutex sync.Mutex
-
-	chT := make(chan todo)
-	chTs := make(chan []todo, 1)
-
-	for i := 1; i <= amount; i++ {
-		go func(j int) {
-			var t todo
-
-			u := url + strconv.Itoa(j)
-
-			log("GET", u)
-
-			r, _ := http.Get(u)
-			b, _ := ioutil.ReadAll(r.Body)
-
-			defer r.Body.Close()
-
-			json.Unmarshal(b, &t)
-
-			chT <- t
-		}(i)
-
-		select {
-		case t := <-chT:
-			mutex.Lock()
-			todos = append(todos, t)
-			mutex.Unlock()
-		}
-	}
-
-	chTs <- todos
-
-	return chTs
-}
-
-func p(ts []todo) {
-	for _, t := range ts {
-		var nT todo
-
-		log("POST", url)
-
-		bT, _ := json.Marshal(t)
-		strT := string(bT)
-
-		r, _ := http.Post(url, "application/json", bytes.NewBufferString(strT))
-		b, _ := ioutil.ReadAll(r.Body)
-		r.Body.Close()
-
-		json.Unmarshal(b, &nT)
-
-		bNT, _ := json.Marshal(nT)
-
-		ioutil.WriteFile(strconv.Itoa(nT.ID)+".json", bNT, 0644)
-	}
+type photo struct {
+	AlbumID     int    `json:"albumId"`
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	URL         string `json:"url"`
+	ThumbailURL string `json:"thumbnailUrl"`
 }
 
 func main() {
+	var photos []photo
+
 	start := time.Now()
 
-	select {
-	case ts := <-g():
-		bTs, _ := json.Marshal(ts)
-		ioutil.WriteFile(fileName, bTs, 0644)
+	for i := 1; i <= amount; i++ {
+		var p photo
 
-		p(ts)
+		u := url + strconv.Itoa(i)
 
-		fmt.Println(time.Since(start))
+		fmt.Printf("-> %s\n", u)
+
+		r, _ := http.Get(u)
+		b, _ := ioutil.ReadAll(r.Body)
+
+		json.Unmarshal(b, &p)
+
+		photos = append(photos, p)
+
+		r.Body.Close()
 	}
+
+	fmt.Println(time.Since(start))
 }
