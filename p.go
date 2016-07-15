d07ac47da4d90fb078b9b49056ce3c669e1e5c46
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-07-15 07:59:03 -0300

    .

--- p.go
+++ p.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strings"
+)
+
+type httpCb func(w http.ResponseWriter, r *http.Request)
+
+func newRouter() *router {
+	return &router{
+		Name: "rrr",
+		Fns:  make(map[string]httpCb),
+	}
+}
+
+type router struct {
+	Name string
+	Fns  map[string]httpCb
+}
+
+func (rou *router) on(url string, cb httpCb) {
+	locURL := strings.Trim(url, " ")
+	locURL = strings.ToLower(locURL)
+
+	rou.Fns[locURL] = cb
+}
+
+func (rou *router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h := w.Header()
+	h.Add("abc", "123")
+	h.Add("Content-Type", "application/json")
+
+	for k, v := range rou.Fns {
+		if k == r.URL.Path {
+			v(w, r)
+		}
+	}
+}
 
 func main() {
-	fmt.Println("!")
+	r := newRouter()
+
+	r.on("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("!"))
+	})
+
+	r.on("/wtf", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("yo!"))
+	})
+
+	http.ListenAndServe(":1234", r)
 }
