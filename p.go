a3b129e10069e6534e5db07af4e663031b3143d8
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-07-14 22:16:53 -0300

    .

--- p.go
+++ p.go
@@ -1,27 +1,7 @@
 package main
 
-import (
-	"net/http"
-
-	ws "golang.org/x/net/websocket"
-)
-
-type msg struct {
-	Txt string `json:"txt"`
-}
+import "fmt"
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir(".")))
-
-	http.Handle("/ws", ws.Handler(func(client *ws.Conn) {
-		var m msg
-
-		if err := ws.JSON.Receive(client, &m); err != nil {
-			panic(err)
-		}
-
-		ws.JSON.Send(client, msg{Txt: m.Txt + "!"})
-	}))
-
-	http.ListenAndServe(":3456", nil)
+	fmt.Println("!")
 }
