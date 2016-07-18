87742fb9f4c767b50e0216c894e816599da8b904
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-07-18 08:19:31 -0300

    .

--- p.go
+++ p.go
@@ -2,32 +2,22 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"golang.org/x/net/websocket"
 )
-
-func received(c *websocket.Conn) <-chan string {
-	strCh := make(chan string, 1)
-
-	var m string
-
-	websocket.Message.Receive(c, &m)
-
-	strCh <- m
-
-	return strCh
-}
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	http.Handle("/ws", websocket.Handler(func(c *websocket.Conn) {
 		c.Write([]byte("yo!"))
 
+		time.Sleep(1 * time.Second)
+
 		for {
-			select {
-			case msg := <-received(c):
-				c.Write([]byte(msg + "!"))
-			}
+			var m string
+			websocket.Message.Receive(c, &m)
+			c.Write([]byte(m + "!"))
 		}
 	}))
 
