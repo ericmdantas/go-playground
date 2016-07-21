3dda6164793c94d93c911806d1f3b210e1844a47
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-07-20 21:30:17 -0300

    .

--- p.go
+++ p.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"golang.org/x/net/websocket"
 )
 
-type mouse struct {
-	OffsetX int `json:"offsetX"`
-	OffsetY int `json:"offsetY"`
-	Amount  int `json:"amount"`
+type info struct {
+	X      int    `json:"x"`
+	Y      int    `json:"y"`
+	Amount int    `json:"amount"`
+	Hash   string `json:"hash"`
 }
 
 const port = ":1234"
@@ -19,9 +23,18 @@ func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	http.Handle("/ws", websocket.Handler(func(c *websocket.Conn) {
 		for {
-			var m mouse
-			websocket.JSON.Receive(c, &m)
-			websocket.JSON.Send(c, m)
+			var i info
+
+			h := md5.New()
+
+			websocket.JSON.Receive(c, &i)
+
+			bI, _ := json.Marshal(i)
+
+			h.Write(bI)
+			i.Hash = hex.EncodeToString(h.Sum(nil))
+
+			websocket.JSON.Send(c, i)
 		}
 	}))
 
