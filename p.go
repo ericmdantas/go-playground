76d4f326242d5b37e9284eba25d2674183c63549
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-07-21 21:21:45 -0300

    .

--- p.go
+++ p.go
@@ -19,22 +19,33 @@ type info struct {
 
 const port = ":1234"
 
+func doEet(c *websocket.Conn) <-chan info {
+	ch := make(chan info, 1)
+
+	var i info
+
+	h := md5.New()
+
+	websocket.JSON.Receive(c, &i)
+
+	bI, _ := json.Marshal(i)
+
+	h.Write(bI)
+	i.Hash = hex.EncodeToString(h.Sum(nil))
+
+	ch <- i
+
+	return ch
+}
+
 func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	http.Handle("/ws", websocket.Handler(func(c *websocket.Conn) {
 		for {
-			var i info
-
-			h := md5.New()
-
-			websocket.JSON.Receive(c, &i)
-
-			bI, _ := json.Marshal(i)
-
-			h.Write(bI)
-			i.Hash = hex.EncodeToString(h.Sum(nil))
-
-			websocket.JSON.Send(c, i)
+			select {
+			case i := <-doEet(c):
+				websocket.JSON.Send(c, i)
+			}
 		}
 	}))
 
